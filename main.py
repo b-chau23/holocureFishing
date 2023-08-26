37acc2fc7ee432108155b99272b7e1a8ecc0ad6b
b-chau23 <bowiechau2301@gmai.com>
2023-08-25 22:07:25 -0400

    Comments

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from mss import mss
 
 
 def fish(search):
-    print("Start")
+    print("Start!")
     spot = {'left': search[0], 'top': search[1], 'width': search[2], 'height': search[3]}
     with mss() as sct:
         while True:
+            # Constantly screenshot 10 pixels in the frame/fishing zone
             pixel = sct.grab(spot)
             pixels = [pixel.pixel(i, 0) for i in range(10)]
-
+            # Perform the associated action depending on what colour was detected
             for px in pixels:
                 if px in ARROW_COLORS:
                     press_button(ARROW_COLORS[px])
@@ -19,9 +20,12 @@ def fish(search):
 
 
 def set_up():
+    print("Ready!")
     search = None
     while not search:
         try:
+            # Identify center of frame and offset the x by 20 to act as a buffer zone
+            # give an additinal 10 pixels in width as extra buffer
             x, y = auto.locateCenterOnScreen("frame.png", confidence=0.7)
             search = (x - 20, y, 10, 1)
         except TypeError:
@@ -30,13 +34,17 @@ def set_up():
 
 
 def press_button(action):
+    # Hey it's that global variable from if __name__ == "__main__"
     global reset
+    # Cancel the existing thread if it is alive
     if reset.is_alive():
         reset.cancel()
+    # If 2 seconds pass without a button press, assume fishing has been completed
+    # and recast the rod
     reset = Timer(2.0, recast_rod)
     reset.start()
 
-    # I know press() exists but it only works with the sleep
+    # I know press() exists but the sleep is necessary for functionality
     auto.keyDown(action)
     time.sleep(0.1)
     auto.keyUp(action)
@@ -51,6 +59,7 @@ def recast_rod():
 
 
 if __name__ == "__main__":
+    # Dictionary for colors and their associated actions
     ARROW_COLORS = {
         (45, 235, 43): 'd',
         (245, 197, 67): 'a',
@@ -58,7 +67,6 @@ if __name__ == "__main__":
         (225, 50, 50): 'w',
         (174, 49, 208): 'space'
     }
-    start, stop = time.time(), time.time()
     # initialize a global variable for press_button
     # also just so happens to start the program
     reset = Timer(0.0, set_up)
